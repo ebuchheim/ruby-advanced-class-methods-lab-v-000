fc47665554ae8e4e80a52342ef6eb5363ff8e797
ebuchheim <emma.buchheim@gmail.com>
2019-01-21 12:03:46 -0800

    add song class logic

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,63 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+
+
+  def self.find_by_name(song_name)
+    @@all.find {|song| song.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song)
+    if self.find_by_name(song)
+      self.find_by_name(song)
+    else
+      self.create_by_name(song)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by! {|song| song.name.downcase}
+  end
+
+  def self.new_from_filename(filename)
+    song_info = filename.split(/\s-\s|\./)
+    artist_name = song_info[0]
+    song_name = song_info[1]
+    song_format = song_info[2]
+    song = self.new_by_name(song_name)
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song_info = filename.split(/\s-\s|\./)
+    artist_name = song_info[0]
+    song_name = song_info[1]
+    song_format = song_info[2]
+    song = self.create_by_name(song_name)
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
